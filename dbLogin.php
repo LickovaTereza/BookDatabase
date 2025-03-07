a093b535722c6623c55e3a0b712ea16f33d3782a
LickovaTereza <tereza.lickova@icloud.com>
2025-03-07 08:18:05 +0100

    new project

---
+++ dbLogin.php
@@ -0,0 +1,15 @@
+<?php
+
+// return [
+//   "host" => "localhost",
+//   "dbname" => "tereza1741246411",
+//   "username" => "tereza1741246411",
+//   "password" => "heslo1741"
+// ];
+
+return [
+  "host" => "localhost",
+  "dbname" => "zkouska",
+  "username" => "root",
+  "password" => ""
+];
